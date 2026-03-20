f84e4eb23df996079267e8196470456fce183afa
Hassan4082827 <nurminakhatun5659@gmail.com>
2026-03-20 17:23:25 +0530

    Add files via upload

--- interactivity/script.js
+++ interactivity/script.js
@@ -1,1 +1,22 @@
+function toggleMenu() {
+            ls.classList.toggle('slide-in');
+            overlay.classList.toggle('active');
+        }
+let startX = 0;
 
+document.body.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].clientX;
+});
+
+document.body.addEventListener('touchend', (e) => {
+    let endX = e.changedTouches[0].clientX;
+    const menu = document.getElementById('ls');
+
+    if ((endX - startX) > 70 && menu.classList.contains('slide-in')) {
+        toggleMenu();
+    }
+
+    if ((startX - endX) > 70 && !menu.classList.contains('slide-in')) {
+        toggleMenu();
+    }
+});
